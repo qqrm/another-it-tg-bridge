12a2ab8d8f50f61cb082034d10e113506b316125
Alexey <4681325+qqrm@users.noreply.github.com>
2025-08-19 11:48:18 +0300

    chore: install wrkflw

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,3 +13,13 @@ fi
 
 rustup component add rustfmt clippy
 cargo binstall cargo-machete --no-confirm
+
+if ! command -v wrkflw >/dev/null 2>&1; then
+  cargo binstall wrkflw@0.7.0 --no-confirm || {
+    tmpdir="$(mktemp -d)"
+    curl -L "https://github.com/bahdotsh/wrkflw/releases/download/v0.7.0/wrkflw-v0.7.0-linux-x86_64.tar.gz" \
+      | tar -xz -C "$tmpdir"
+    install -m 755 "$tmpdir/wrkflw" "$HOME/.cargo/bin/"
+    rm -rf "$tmpdir"
+  }
+fi
